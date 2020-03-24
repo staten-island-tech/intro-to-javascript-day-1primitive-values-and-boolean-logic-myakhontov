68d307feba87b6c8afb609ff65b4f2a6db2bdbb1
myakhontov <michael.yakhontov23@sitechhs.com>
2020-03-24 15:00:11 -0400

    first day lesson

---
+++ index.js
@@ -0,0 +1,50 @@
+let string = "strings" ;    //variable declaration, after = definitions
+let sentence = "Hello " + "Junshen";
+let sentence2 = + " and Emu";
+//console.log(sentence + sentence2);
+
+const longSentence = `${sentence} and ${sentence2} are amazing!` //Yemplate Literal or Template String
+
+///////////////////////////Numbers or Integers
+
+let year = 2020;
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farfuture = 2020 * 50;
+
+/////////////////////////Booleans
+let trueStatement = true; //1
+let falseStatement = false; //0
+
+
+/////////Null and Undefined
+let Ron; //yes declaration, no definition, undefined
+//console.log(Sarah) not defined, may be spelling error or no declaration, NULL
+
+/////////////////////Boolean Logic
+let student = "Caroline";
+let age = 15;
+//console.log(student === "Caroline"); //true
+//console.log(student === "caroline"); //false
+//console.log(age === "15"); //false, triple is exact, double converts to same type
+//console.log(age < 17 || age > 19); //true, OR
+//console.log(age < 17 && age > 19); //false, AND
+console.log(15 == "15");
+
+//////////////////////////////////if statements
+
+let school = "Middle";
+
+if(school === "Elementary"){
+    console.log("You are in k-5");
+} else if(school === "Middle"){
+    console.log("You are in 6,7,8")
+} else{
+    console.log("you are in high school")
+}
+
+let ron = "Ron";
+console.log(ron !== "Ema");
+
+
+
